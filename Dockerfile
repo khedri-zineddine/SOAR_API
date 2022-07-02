68733b1dbdedaaef733f6c37bfe77195ff553e7a
zineddine <hz_khedri@esi.dz>
2022-07-02 21:05:08 +0100

    COnfig redis

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM python:3.8
+
 ARG port
 
+#RUN apt-get update
+
+#RUN curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+
+#RUN apt-get update
+#RUN apt-get install -y redis
+#RUN redis-server --daemonize yes
+#RUN service redis-server start
+#RUN redis-cli ping
 COPY . /soar-api
 WORKDIR /soar-api
 RUN pip install -r requirements.txt
@@ -8,4 +18,5 @@ RUN pip install -r requirements.txt
 ENV PORT=$port
 EXPOSE $PORT
 
+#CMD (redis-server &) && redis-cli ping
 CMD flask run --host=0.0.0.0 --port=$PORT
--- app_base.py
+++ app_base.py
@@ -1,15 +1,18 @@
-
-
-
 from flask_classful import FlaskView
 import redis
+import os
+from urllib.parse import urlparse
 import logging
 
+
 class AppBase(FlaskView):
-    def __init__(self,REDIS_URL='127.0.0.1',REDIS_PORT='6379',DB_INDEX=1) -> None:
+    def __init__(self, REDIS_URL="127.0.0.1", REDIS_PORT="6379", DB_INDEX=1) -> None:
         super().__init__()
-        logging.basicConfig(format="{asctime} - {name} - {levelname}:{message}", style='{')
-        self.logger = logging.getLogger(f"{__name__}")
-        self.logger.setLevel(logging.DEBUG)
-        self.redis_conn = redis.Redis(REDIS_URL,REDIS_PORT,DB_INDEX)
-    
+        logging.basicConfig(format="{asctime} - {name} - {levelname}:{message}", style="{")
+        # self.logger = logging.getLogger(f"{__name__}")
+        # self.logger.setLevel(logging.DEBUG)
+        # self.redis_conn = redis.Redis(REDIS_URL,REDIS_PORT,DB_INDEX)
+        url = urlparse(os.environ.get("REDIS_URL"))
+        self.redis_conn = redis.Redis(
+            host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None
+        )
