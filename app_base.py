80a7b67ef3165c606967640d2f604959241463b2
zineddine <hz_khedri@esi.dz>
2022-07-03 11:10:40 +0100

    Add uri redis

--- app_base.py
+++ app_base.py
@@ -8,6 +8,8 @@ REDIS_HEROKU_URL = "ec2-100-26-75-186.compute-1.amazonaws.com"
 REDIS_HEROKU_PORT = "18750"
 REDIS_HEROKU_PASSORD = "pbcd2893860ddd3aaed90da2eac67eee6553cc8bb3531fb218e46f9beda11f823"
 
+REDIS_HEROKU_URI = "redis://:pbcd2893860ddd3aaed90da2eac67eee6553cc8bb3531fb218e46f9beda11f823@ec2-100-26-75-186.compute-1.amazonaws.com:18750"
+
 
 class AppBase(FlaskView):
     def __init__(self, REDIS_URL="127.0.0.1", REDIS_PORT="6379", DB_INDEX=1) -> None:
@@ -15,10 +17,9 @@ class AppBase(FlaskView):
         logging.basicConfig(format="{asctime} - {name} - {levelname}:{message}", style="{")
         self.logger = logging.getLogger(f"{__name__}")
         self.logger.setLevel(logging.DEBUG)
+        # self.redis_conn = redis.Redis(REDIS_HEROKU_URL, REDIS_HEROKU_PORT, DB_INDEX)
+        # self.redis_conn = redis.Redis(host=REDIS_HEROKU_URL, port=REDIS_HEROKU_PORT, password=REDIS_HEROKU_PASSORD, db=DB_INDEX)
+        url = urlparse(REDIS_HEROKU_URI)
         self.redis_conn = redis.Redis(
-            host=REDIS_HEROKU_URL, port=REDIS_HEROKU_PORT, password=REDIS_HEROKU_PASSORD, db=DB_INDEX
+            host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None
         )
-        # url = urlparse(os.environ.get("REDIS_URL"))
-        # self.redis_conn = redis.Redis(
-        #    host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None
-        # )
