6ce670dfb40cc72eff7b6e42996c8edec02bf9f9
zineddine <hz_khedri@esi.dz>
2022-07-03 10:54:38 +0100

    Update redis

--- app_base.py
+++ app_base.py
@@ -6,6 +6,7 @@ import logging
 
 REDIS_HEROKU_URL = "ec2-100-26-75-186.compute-1.amazonaws.com"
 REDIS_HEROKU_PORT = "18750"
+REDIS_HEROKU_PASSORD = "pbcd2893860ddd3aaed90da2eac67eee6553cc8bb3531fb218e46f9beda11f823"
 
 
 class AppBase(FlaskView):
@@ -14,7 +15,9 @@ class AppBase(FlaskView):
         logging.basicConfig(format="{asctime} - {name} - {levelname}:{message}", style="{")
         self.logger = logging.getLogger(f"{__name__}")
         self.logger.setLevel(logging.DEBUG)
-        self.redis_conn = redis.Redis(REDIS_HEROKU_URL, REDIS_HEROKU_PORT, DB_INDEX)
+        self.redis_conn = redis.Redis(
+            host=REDIS_HEROKU_URL, port=REDIS_HEROKU_PORT, password=REDIS_HEROKU_PASSORD, db=DB_INDEX
+        )
         # url = urlparse(os.environ.get("REDIS_URL"))
         # self.redis_conn = redis.Redis(
         #    host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None
